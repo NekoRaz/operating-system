5769a48c75d6d572bbfd8f56e2f87d745d099691
Tobias Findeisen <tobias_findeisen@me.com>
2018-06-10 16:07:44 +0200

    ich weiß nicht was ich gemacht hab

--- Ex4/vorlage4/ult.c
+++ Ex4/vorlage4/ult.c
@@ -18,21 +18,27 @@ array_hdr_t queue;
 /* thread control block */
 typedef struct tcb_s
 {
-	ucontext_t caller, gen;
+    ucontext_t caller, gen;
     int tid;
     char* status;
-	void* yield;
-	char mem[STACK_SIZE];
-	/* data needed to restore the context */
+    void* yield;
+    char mem[STACK_SIZE];
+    /* data needed to restore the context */
 } tcb_t;
 
 tcb_t* current_thread;
 
 void ult_init(ult_f f)
 {
-    threadCounter = 0;
-
-
+    threadCounter = 1;
+    // create the new stack
+    current_thread->gen.uc_link = 0;
+    current_thread->gen.uc_stack.ss_flags = 0;
+    current_thread->gen.uc_stack.ss_size = STACK_SIZE;
+    current_thread->gen.uc_stack.ss_sp = current_thread->mem;
+    current_thread->status = "ready";
+    current_thread->tid = threadCounter;
+    
 }
 
 int ult_spawn(ult_f f)
@@ -58,13 +64,13 @@ int ult_spawn(ult_f f)
     makecontext(&current_thread->gen, f, 0);
     
     // use threadCounter as thread id
-	return threadCounter;
+    return threadCounter;
 }
 
 void ult_yield()
 {
-	current_thread->status = "wait";
-	swapcontext(&current_thread->gen, &current_thread->caller);
+    current_thread->status = "wait";
+    swapcontext(&current_thread->gen, &current_thread->caller);
 }
 
 void ult_exit(int status)
@@ -72,10 +78,10 @@ void ult_exit(int status)
 
 int ult_join(int tid, int* status)
 {
-	return -1;
+    return -1;
 }
 
 ssize_t ult_read(int fd, void* buf, size_t size)
 {
-	return 0;
+    return 0;
 }
